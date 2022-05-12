8f393d46e27a61b9ca1be0080c3eae12a536ee94
Jheison <jheisong@gmail.com>
2022-05-12 16:40:24 -0300

    Add project files.

---
+++ PV.Sample01.Api/Application/Receivables/Models/ReceivableUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PV.Sample01.Api.Models.Receivables
+{
+    public class ReceivableUnit
+    {
+        public string Product { get; set; }
+        public string Flag { get; set; }
+        public DateTime Date { get; set; }
+        public string Document { get; set; }
+        public decimal GrossValue { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Value { get; set; }
+    }
+}
